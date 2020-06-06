3d2585fe2642fc0d42b9a320c73adee8ab1530c1
Christopher Acevedo <cacevedo@thedataage.com>
2020-06-05 23:09:59 -0600

    integracion de backend de noticia

--- src/views/Noticias/Detalle/Detalle.js
+++ src/views/Noticias/Detalle/Detalle.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState, useEffect, Fragment } from "react";
+import Fecha from './Fecha'
 // reactstrap components
 import {
   Button,
@@ -12,6 +12,7 @@ import {
   InputGroup,
   InputGroupAddon,
   Input,
+  Spinner,
 } from "reactstrap";
 import Request from "../../../service/Request";
 // core components
@@ -19,6 +20,9 @@ import Request from "../../../service/Request";
 function Detalle(props) {
   const [vlImage, setVlImage] = useState("");
   const [txNoticia, setTxNoticia] = useState(null);
+  const [title, setTitle] = useState("");
+  const [loading, setloading] = useState(true);
+  const [subtitle, setSubtitle] = useState("");
 
   useEffect(() => {
     let mounted = true;
@@ -29,7 +33,10 @@ function Detalle(props) {
         //console.log(res);
         if (mounted) {
           setTxNoticia(res.txNoticia);
-          setVlImage(res.vlImage);
+          setVlImage(res.idNoticiaHeaderNavigation.vlImage);
+          setTitle(res.idNoticiaHeaderNavigation.nbNoticia);
+          getcategoria(res.idNoticiaHeaderNavigation.idCategoria,res.fhRegistro);
+          setloading(false);
         }
       })
       .catch((err) => {
@@ -39,135 +46,150 @@ function Detalle(props) {
     return () => (mounted = false);
   }, [props.match.params.id]);
 
+  const getcategoria = (idCategoria,fecha) => {
+    const f=new Fecha(fecha)
+    const req = new Request();
+    req
+      .listGET("/api/categorias/" + idCategoria)
+      .then((res) => {
+        //console.log(res);
+        setSubtitle(f.getFecha()+" | "+res.tpCategoria + " | " + res.nbCategoria);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <div style={{ marginLeft: "3.5rem", marginRight: "3.5rem" }}>
         <Row>
           <Col md="9">
-            <h2 className="title text-center">
-              Médicos de Nicaragua en lucha permanente contra la pandemia de la
-              Covid-19
-            </h2>
-            <hr></hr>
-            <h6 className="mt-2 mb-2">
-              Lunes 1 de Junio 2020 | Noticias | Covid 19
-            </h6>
-            <img alt="" src={vlImage} width="100%" height="25%" />
-            <Button
-              className="mt-4 mb-2"
-              color="primary"
-              onClick={() => {
-                props.history.goBack();
-              }}
-            >
-              Volver al Menú
-            </Button>
-            <Row>
-              <Col className="mb-4 d-flex flex-wrap justify-content-center">
-                <h6>
-                  Comparte esta noticia:
-                  <Button
-                    className="ml-2 btn-icon btn-round"
-                    color="success"
-                    href="https://www.fb.com/planetaverde.edu.ni/"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      window.open("https://www.fb.com/planetaverde.edu.ni");
-                    }}
-                  >
-                    <i className="fab fa-whatsapp"></i>
-                  </Button>
-                  <Button
-                    className="ml-2 btn-icon btn-round"
-                    color="info"
-                    href="https://www.fb.com/planetaverde.edu.ni/"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      window.open("https://www.fb.com/planetaverde.edu.ni");
-                    }}
-                  >
-                    <i className="fab fa-facebook-square"></i>
-                  </Button>
-                  <Button
-                    className=" ml-2 btn-icon btn-round"
-                    color="danger"
-                    href="https://www.fb.com/planetaverde.edu.ni/"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      window.open("https://www.fb.com/planetaverde.edu.ni");
-                    }}
-                  >
-                    <i className="fab fa-instagram"></i>
-                  </Button>
-                  <Button
-                    className=" ml-2 btn-icon btn-round"
-                    color="info"
-                    href="https://www.fb.com/planetaverde.edu.ni/"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      window.open("https://www.fb.com/planetaverde.edu.ni");
-                    }}
-                  >
-                    <i className="fab fa-twitter"></i>
-                  </Button>
-                </h6>
-              </Col>
-            </Row>
-            <Row>
-              <Col dangerouslySetInnerHTML={{ __html: txNoticia }}>
-              </Col>
-            </Row>
-            <Row>
-              <Col className=" mt-4 mb-4 d-flex flex-wrap justify-content-center">
-                <h6>
-                  Comparte esta noticia:
-                  <Button
-                    className="ml-2 btn-icon btn-round"
-                    color="success"
-                    href="https://www.fb.com/planetaverde.edu.ni/"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      window.open("https://www.fb.com/planetaverde.edu.ni");
-                    }}
-                  >
-                    <i className="fab fa-whatsapp"></i>
-                  </Button>
-                  <Button
-                    className="ml-2 btn-icon btn-round"
-                    color="info"
-                    href="https://www.fb.com/planetaverde.edu.ni/"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      window.open("https://www.fb.com/planetaverde.edu.ni");
-                    }}
-                  >
-                    <i className="fab fa-facebook-square"></i>
-                  </Button>
-                  <Button
-                    className=" ml-2 btn-icon btn-round"
-                    color="danger"
-                    href="https://www.fb.com/planetaverde.edu.ni/"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      window.open("https://www.fb.com/planetaverde.edu.ni");
-                    }}
-                  >
-                    <i className="fab fa-instagram"></i>
-                  </Button>
-                  <Button
-                    className=" ml-2 btn-icon btn-round"
-                    color="info"
-                    href="https://www.fb.com/planetaverde.edu.ni/"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      window.open("https://www.fb.com/planetaverde.edu.ni");
-                    }}
-                  >
-                    <i className="fab fa-twitter"></i>
-                  </Button>
-                </h6>
-              </Col>
-            </Row>
+            {loading ? (
+              <Spinner />
+            ) : (
+              <Fragment>
+                <h2 className="title text-center">{title}</h2>
+                <hr></hr>
+                <h6 className="mt-2 mb-2">{subtitle}</h6>
+                <img alt="" src={vlImage} width="100%" height="25%" />
+                <Button
+                  className="mt-4 mb-2"
+                  color="primary"
+                  onClick={() => {
+                    props.history.goBack();
+                  }}
+                >
+                  Volver al Menú
+                </Button>
+                <Row>
+                  <Col className="mb-4 d-flex flex-wrap justify-content-center">
+                    <h6>
+                      Comparte esta noticia:
+                      <Button
+                        className="ml-2 btn-icon btn-round"
+                        color="success"
+                        href="https://www.fb.com/planetaverde.edu.ni/"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          window.open("https://www.fb.com/planetaverde.edu.ni");
+                        }}
+                      >
+                        <i className="fab fa-whatsapp"></i>
+                      </Button>
+                      <Button
+                        className="ml-2 btn-icon btn-round"
+                        color="info"
+                        href="https://www.fb.com/planetaverde.edu.ni/"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          window.open("https://www.fb.com/planetaverde.edu.ni");
+                        }}
+                      >
+                        <i className="fab fa-facebook-square"></i>
+                      </Button>
+                      <Button
+                        className=" ml-2 btn-icon btn-round"
+                        color="danger"
+                        href="https://www.fb.com/planetaverde.edu.ni/"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          window.open("https://www.fb.com/planetaverde.edu.ni");
+                        }}
+                      >
+                        <i className="fab fa-instagram"></i>
+                      </Button>
+                      <Button
+                        className=" ml-2 btn-icon btn-round"
+                        color="info"
+                        href="https://www.fb.com/planetaverde.edu.ni/"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          window.open("https://www.fb.com/planetaverde.edu.ni");
+                        }}
+                      >
+                        <i className="fab fa-twitter"></i>
+                      </Button>
+                    </h6>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col dangerouslySetInnerHTML={{ __html: txNoticia }}></Col>
+                </Row>
+                <Row>
+                  <Col className=" mt-4 mb-4 d-flex flex-wrap justify-content-center">
+                    <h6>
+                      Comparte esta noticia:
+                      <Button
+                        className="ml-2 btn-icon btn-round"
+                        color="success"
+                        href="https://www.fb.com/planetaverde.edu.ni/"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          window.open("https://www.fb.com/planetaverde.edu.ni");
+                        }}
+                      >
+                        <i className="fab fa-whatsapp"></i>
+                      </Button>
+                      <Button
+                        className="ml-2 btn-icon btn-round"
+                        color="info"
+                        href="https://www.fb.com/planetaverde.edu.ni/"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          window.open("https://www.fb.com/planetaverde.edu.ni");
+                        }}
+                      >
+                        <i className="fab fa-facebook-square"></i>
+                      </Button>
+                      <Button
+                        className=" ml-2 btn-icon btn-round"
+                        color="danger"
+                        href="https://www.fb.com/planetaverde.edu.ni/"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          window.open("https://www.fb.com/planetaverde.edu.ni");
+                        }}
+                      >
+                        <i className="fab fa-instagram"></i>
+                      </Button>
+                      <Button
+                        className=" ml-2 btn-icon btn-round"
+                        color="info"
+                        href="https://www.fb.com/planetaverde.edu.ni/"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          window.open("https://www.fb.com/planetaverde.edu.ni");
+                        }}
+                      >
+                        <i className="fab fa-twitter"></i>
+                      </Button>
+                    </h6>
+                  </Col>
+                </Row>
+              </Fragment>
+            )}
+
             <h3 className="text-primary title">Puede interesarle</h3>
             <hr></hr>
             <Row>
